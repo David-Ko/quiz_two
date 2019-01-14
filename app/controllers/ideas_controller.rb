83d6a44b3c8bd0ede77f0b4e0cc2b84f1ae048fa
David Ko <dkoflow@gmail.com>
2019-01-14 10:29:28 -0800

    Completed CRUD for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,5 @@
 class IdeasController < ApplicationController
+    before_action :find_idea, only: [:show, :destroy, :edit, :update]
 
     def index
         @ideas = Idea.all.order(created_at: :desc)
@@ -6,5 +7,47 @@ class IdeasController < ApplicationController
 
     def new
         @idea = Idea.new
+    end
+
+    def create
+        @idea = Idea.new idea_params
+        # @idea.user = current_user
+        @idea.save
+            redirect_to ideas_path
+    end
+
+    def show
+        # @idea = Idea.find params[:id]
+    end
+
+    def destroy
+        # @idea = Idea.find params[:id]
+        @idea.destroy
+        redirect_to ideas_path
+    end
+
+    def edit
+        # @idea = Idea.find params[:id]
+    end
+
+    def update
+        # @idea = Idea.find params[:id]
+        if @idea.update idea_params
+            redirect_to idea_path(@idea.id)
+        else 
+            render :edit
+        end
+    end
+
+
+
+    private 
+
+    def idea_params
+        params.require(:idea).permit(:title, :body)
+    end
+
+    def find_idea
+        @idea = Idea.find params[:id]
     end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,10 @@
 class Idea < ApplicationRecord
+
+    validates(
+        :title, presence: { message: "Title ust be present" }
+    )
+
+    validates(
+        :body, presence: { message: "Body must be present" }
+    )
 end
